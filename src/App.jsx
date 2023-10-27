91f8d014253727e41e8e0c2161dd3521b525ebe2
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2023-10-27 15:51:20 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import logo from "/logo.png";
+import "./App.css";
+import { getRandomWord } from "./utils";
+import { useState } from "react";
+
+function App() {
+  // currWord is the current secret word for this round. Update this with the updater function after each round.
+  const [currWord, setCurrentWord] = useState(getRandomWord());
+  // guessedLetters stores all letters a user has guessed so far
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  // Add additional states below as required.
+
+  const generateWordDisplay = () => {
+    const wordDisplay = [];
+    // for...of is a string and array iterator that does not use index
+    for (let letter of currWord) {
+      if (guessedLetters.includes(letter)) {
+        wordDisplay.push(letter);
+      } else {
+        wordDisplay.push("_");
+      }
+    }
+    return wordDisplay.toString();
+  };
+
+  // create additional function to power the
+
+  return (
+    <>
+      <div>
+        <img src={logo} className="logo" alt="Rocket logo" />
+      </div>
+      <div className="card">
+        <h1>Guess The Word 🚀</h1>
+        <h3>Word Display</h3>
+        {generateWordDisplay()}
+        <h3>Guessed Letters</h3>
+        {this.state.guessedLetters.length > 0
+          ? this.state.guessedLetters.toString()
+          : "-"}
+        <br />
+        <h3>Input</h3>
+        {/* Insert form element here */}
+      </div>
+    </>
+  );
+}
+
+export default App;
