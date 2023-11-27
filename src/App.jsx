9e4eaa0ac39b75ab9f43949378b65413e6a0ec35
benleong94 <bendemeerben@gmail.com>
2023-11-27 22:10:06 +0800

    Edited

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [currLetter, setCurrLetter] = useState('');
   const [alreadyGuessedLetterMsg, setAlrMessage] = useState('');
   const [guessesLeft, setGuessesLeft] = useState(10);
+  const [winState, setWinState] = useState(false);
 
   const generateWordDisplay = () => {
     const wordDisplay = [];
@@ -20,7 +21,8 @@ function App() {
         wordDisplay.push("_");
       }
     }
-    return wordDisplay.toString();
+
+    return wordDisplay.toString()
   };
 
   const handleInputChange = (event) => {
@@ -31,11 +33,21 @@ function App() {
     if (guessedLetters.includes(currLetter)) {
       setAlrMessage("You have guessed this letter before. Try Again!") 
     } else {
+      setAlrMessage("");
       setGuessedLetters(prevItems => [...prevItems, currLetter]);   
       if(!currWord.includes(currLetter)){
         setGuessesLeft(prevState => prevState - 1)
+      } 
+    }
+  }
+
+  const checkWin = (wordArray) => {
+    for (let letter of wordArray) {
+      if (!guessedLetters.includes(letter)) {
+        return false
       }
     }
+    return true; 
   }
 
   const displayImage = () => {
@@ -43,37 +55,53 @@ function App() {
     return <img src={path}  alt="10 Guesses" />;
   }
 
+  const resetGame = () => {
+    setCurrentWord(getRandomWord());
+    setGuessedLetters([]);
+    setCurrLetter('');
+    setAlrMessage('');
+    setGuessesLeft(10);
+    setWinState(false);
+  }
+
+  useEffect(() => {
+    checkWin(currWord)? setWinState(true): setWinState(false)
+  }, [guessedLetters])
+
   return (
     <>
       <div>
         <img src={logo} className="logo" alt="Rocket logo" />
       </div>
       <div>
-        <h2>Word To Guess:</h2>
+        <h2>Guess The Word:</h2>
         <div className="hidden-word">{generateWordDisplay()}</div>
+        {guessesLeft == 0 ? <h3 style={{ color: "red" }}>You Lost!</h3> : null}
+        {winState ? <h3 style={{ color: "#90EE90" }}>You Won!</h3> : null}
+        {winState || guessesLeft == 0 ? (
+          <button onClick={resetGame}> Reset Game!</button>
+        ) : null}
       </div>
       <div className="card">
         <div className="right-side">
           <div>{displayImage()}</div>
         </div>
         <div className="left-side">
+          <div className="number-guesses">Guesses Left: {guessesLeft}</div>
           <div className="guessed-letters">
             <h3>Guessed Letters</h3>
             <div>
               {guessedLetters.length > 0 ? guessedLetters.toString() : "-"}
             </div>
           </div>
-          <div className="number-guesses">
-            Guesses Left: {guessesLeft}
-            {guessesLeft == 0 ? (
-              <h3 style={{ color: "red" }}>You Lost!</h3>
-            ) : null}
-          </div>
           <div className="input-container">
             <h3>Input</h3>
-            {alreadyGuessedLetterMsg ? (
-              <h5>{alreadyGuessedLetterMsg}</h5>
-            ) : null}
+            <div className="already-guessed">
+              {alreadyGuessedLetterMsg ? (
+                <h5>{alreadyGuessedLetterMsg}</h5>
+              ) : null}
+            </div>
+
             <input
               type="text"
               value={currLetter}
