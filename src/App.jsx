6db6ba11192a41871b687f2bab3e1d663f9a235e
jasperteo <jaspertzj@outlook.sg>
2023-11-28 15:45:27 +0800

    added input and error validation

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,10 @@ function App() {
   const [currWord, setCurrentWord] = useState(getRandomWord());
   // guessedLetters stores all letters a user has guessed so far
   const [guessedLetters, setGuessedLetters] = useState([]);
+  const [errorState, setErrorState] = useState(false);
 
   // Add additional states below as required.
+  const [guess, setGuess] = useState("");
 
   const generateWordDisplay = () => {
     const wordDisplay = [];
@@ -25,7 +27,16 @@ function App() {
   };
 
   // create additional function to power the
-
+  const handleSubmit = (word) => {
+    if (!guessedLetters.includes(word) && !!word) {
+      setErrorState(false);
+      setGuessedLetters((x) => [...x, word]);
+      setGuess("");
+    } else {
+      setErrorState(true);
+      setGuess("");
+    }
+  };
   return (
     <>
       <div>
@@ -36,12 +47,19 @@ function App() {
         <h3>Word Display</h3>
         {generateWordDisplay()}
         <h3>Guessed Letters</h3>
-        {this.state.guessedLetters.length > 0
-          ? this.state.guessedLetters.toString()
-          : "-"}
+        {guessedLetters.toString()}
         <br />
         <h3>Input</h3>
         {/* Insert form element here */}
+        <input
+          placeholder="Type in letter here"
+          maxLength="1"
+          pattern="[a-zA-Z]"
+          value={guess}
+          onChange={(e) => setGuess(e.target.value)}
+        />
+        <button onClick={() => handleSubmit(guess)}>Guess!</button>
+        <p>{errorState ? "Invalid Input!" : ""}</p>
       </div>
     </>
   );
